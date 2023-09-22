ab4151e358be85df9cf71279440d4f4036f74b8f
Joshua Strickland <strickki@Joshuas-Air.attlocal.net>
2023-09-22 16:09:52 -0500

    able to extract names from excel sheet

--- playerSearch.py
+++ playerSearch.py
@@ -1,12 +1,37 @@
 import requests
 import json
 import pandas as pd
+import tkinter as tk
 from openpyxl import load_workbook
+from tkinter import filedialog
 
 auth_token = "eyJhbGciOiJSUzUxMiJ9.eyJpc3MiOiJodHRwczovL2R1cHIuZ2ciLCJpYXQiOjE2OTUyMTk2MDEsImp0aSI6IjY5NDA2OTAzNDQiLCJleHAiOjE2OTc4MTE2MDEsInN1YiI6ImMzUnlhV05yT1RjME0wQjVZV2h2Ynk1amIyMD0ifQ.MUCxJ06uYNC-u7zVrTBhmecFxsIEriQyZ8LEAPL9ZB8mYr1eH21ufJAUwvwwWpHu0Ag7NrtKug9l57BtwePIDWdXpq7xsJkfar_BBIwu1QaKsJlxakCjk2ZHdLHRopXMLOE5k_Q9dnmwSys_Qq203tVfyETQEBfWszCdY8zO4E44_N89cg4hUszVh4khxMaXTW41nRg6UyaDbT2DV3nSzQObSiCNsotiRJxQY6zZtMupmHnbzpVMaFycNwFh865Oy0Sy8mjvXi_-hIS0Fdgml6zJLqg28FENlQwsKfaLlT2OzEl70LrG7IFgclUVrolHoDatDv39tgXv7vg5N-yg2A"
 
+# Function to open a file in the system
+tk.Tk().withdraw()
+def selectExcelFile():
+   filepath = filedialog.askopenfilename()
+   return filepath
 
-def get_data(name):
+def extractExcelNames(filePath):
+    # TODO: Excel functionality 
+    workbook = load_workbook(filePath) #filename=filePath)
+    sheet = workbook.active
+    sheet = workbook['Sheet1']
+    firstNames = []
+    for i in range(2, sheet.max_row+1):
+        firstNames.append(sheet.cell(row=i,column=1).value)
+    lastNames = []
+    for i in range(2, sheet.max_row+1):
+        lastNames.append(sheet.cell(row=i,column=2).value)
+    fullNames = []
+    for i in range(len(firstNames)):
+        fullNames.append(firstNames[i] + " " + lastNames[i])
+    
+    for i in range(len(fullNames)):
+        getData(fullNames[i])
+
+def getData(name):
     headers = {
         "Authorization": "Bearer " + auth_token,
         "Content-type": "application/json"
@@ -38,19 +63,14 @@ def get_data(name):
         print("Error with accessing DUPR endpoint")
         exit()
     
-    firstName = response['result']['hits'][0]['firstName']
-    lastName = response['result']['hits'][0]['lastName']
+    fullName = response['result']['hits'][0]['fullName']
+    # lastName = response['result']['hits'][0]['lastName']
     doublesRating = response['result']['hits'][0]['ratings']['doubles']
     singlesRating = response['result']['hits'][0]['ratings']['singles']
     
-    print(firstName + ' ' + lastName + '\'s ' + 'doubles rating is: ' + doublesRating); 
-    print(firstName + ' ' + lastName + '\'s ' + 'singles rating is: ' + singlesRating); 
+    print(fullName +  '\'s ' + 'doubles rating is: ' + doublesRating); 
+    print(fullName + '\'s ' + 'singles rating is: ' + singlesRating); 
     
-    # TODO: Excel functionality 
-    workbook = load_workbook(filename="ERA-Usernames.xlsx")
-    sheet = workbook.active
-    print(sheet["A1"].value)
-   
-
-    
-get_data("rob rhett")
+# get_data(selectExcelFile())
+extractExcelNames("/Users/strickki/Downloads/ERA-Usernames (1).xlsx")
+# get_data("rob rhett")
